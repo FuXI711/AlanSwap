8eba0e7f2923ae411c70df4c9ad0f856184ecf30
seckiller <847315653@qq.com>
2025-10-13 17:49:20 +0800

    流动性池合约同步

---
+++ src/common/abi/abi_constants.go
@@ -0,0 +1,25 @@
+package abi
+
+import "github.com/ethereum/go-ethereum/accounts/abi"
+
+// ABI名称常量
+const (
+	ABIUniswapV2Pair    = "UniswapV2Pair"
+	ABIERC20            = "ERC20"
+	ABIUniswapV2Factory = "UniswapV2Factory"
+)
+
+// 便捷函数 - 获取UniswapV2Pair ABI
+func GetUniswapV2PairABI() abi.ABI {
+	return GetABIManager().MustGetABI(ABIUniswapV2Pair)
+}
+
+// 便捷函数 - 获取ERC20 ABI
+func GetERC20ABI() abi.ABI {
+	return GetABIManager().MustGetABI(ABIERC20)
+}
+
+// 便捷函数 - 获取UniswapV2Factory ABI
+func GetUniswapV2FactoryABI() abi.ABI {
+	return GetABIManager().MustGetABI(ABIUniswapV2Factory)
+}
